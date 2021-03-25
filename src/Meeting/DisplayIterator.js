b5b4b260c40f041d41bb286af3d889a3972e1f52
filipecamargos <filipe.atrabalho@gmail.com>
2021-03-24 19:11:25 -0600

    Change btn style

--- src/Meeting/DisplayIterator.js
+++ src/Meeting/DisplayIterator.js
@@ -5,9 +5,14 @@ const DisplayIterator = (props) => {
     return (
         <div>
           <div className="btnsIterators">
-            <button type="button" className="btn btn-info" onClick={props.refPreviousSetMeetings}>Previous</button>
-            <button type="button" className="btn btn-info" onClick={props.refnextSetMeetings}>Next</button>
+            <button type="button" className="btn btn-info" onClick={props.refPreviousSetMeetings}>
+              <i className="glyphicon glyphicon-chevron-left"></i> 
+            </button>
+            <button type="button" className="btn btn-info" onClick={props.refnextSetMeetings}>
+              <i className="glyphicon glyphicon-chevron-right"></i>
+            </button>
             <span>Meetings {props.displayManager.startPoint + 1} -  {props.displayManager.endPoint} | {props.totalPages} </span>
+            <button type="button" className="">Add New</button> 
           </div>
         </div>
     );
